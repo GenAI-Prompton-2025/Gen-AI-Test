7631398550292e5e4deb1af963cb00050d674e0b
이로움 <JK1234~~>
2025-02-04 17:40:27 +0900

    Add chat-test

---
+++ chat-test.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+
+st.title("ChatBot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"Echo: {prompt}")
+    
+        st.toast('Your edited image was saved!', icon='😍')
